5a860f376b8a875c8cb7c028f9ed61a0c8d4b797
Alex CM <a.cidmargues@students.ephec.be>
2023-10-30 10:30:31 +0100

    ajout settings

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
