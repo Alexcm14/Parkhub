8e71a49857a0d51ff25d29a6ab1421b5445e7554
Alex CM <a.cidmargues@students.ephec.be>
2023-11-01 13:57:33 +0100

    fichier src page personal

--- src/app/pages/personal/personal.module.ts
+++ src/app/pages/personal/personal.module.ts
@@ -1,20 +1,22 @@
+
+
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
-
-import { PersonalPageRoutingModule } from './personal-routing.module';
 
 import { PersonalPage } from './personal.page';
+import { PersonalPageRoutingModule } from './personal-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
+  declarations: [PersonalPage], // Add other components, directives, or pipes if needed
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
-    PersonalPageRoutingModule
+    PersonalPageRoutingModule,
+    IonicStorageModule.forRoot() // Add this line to configure Storage
   ],
-  declarations: [PersonalPage],
 })
 export class PersonalPageModule {}
--- src/app/pages/personal/personal.page.ts
+++ src/app/pages/personal/personal.page.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.page.html',
   styleUrls: ['./personal.page.scss'],
 })
-export class PersonalPage implements OnInit {
 
-  constructor() { }
+export class PersonalPage {
+  nom: string = "";
+  prenom: string = "";
+  email: string = "";
+  motDePasse: string = "";
+  telephone: string = "";
+  registreNational: string = "";
 
-  ngOnInit() {
+  constructor(private storage: Storage) {
+    // Initialize the storage
+    this.initStorage();
   }
 
+  // Initialize Ionic Storage
+  async initStorage() {
+    await this.storage.create();
+  }
+
+  enregistrer() {
+    // Create an object with user data
+    const userData = {
+      nom: this.nom,
+      prenom: this.prenom,
+      email: this.email,
+      motDePasse: this.motDePasse,
+      telephone: this.telephone,
+      registreNational: this.registreNational,
+    };
+
+    // Save user data to local storage
+    this.storage.set('userData', userData).then(() => {
+      console.log('Données enregistrées localement :', userData);
+      this.resetFields();
+    });
+  }
+
+  resetFields() {
+    // Reset input fields after saving data
+    this.nom = '';
+    this.prenom = '';
+    this.email = '';
+    this.motDePasse = '';
+    this.telephone = '';
+    this.registreNational = '';
+  }
 }
