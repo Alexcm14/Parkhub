45d3c571746e6e856d322acb9c4cff9091a2e7b7
mmjahed <he129854@students.ephec.be>
2023-11-10 14:24:23 +0100

    homepage avec google maps
    ajout de google maps, barre de nav pas encore fonctionnel.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { PersonalPageModule } from './pages/personal/personal.module';
+
 
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  },  
   {
     path: 'personal',
     loadChildren: () => import('./pages/personal/personal.module').then( m => m.PersonalPageModule)
-  }
+  },
+
+  
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+ 
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef,OnInit, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { elementAt } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,8 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  map!: GoogleMap;
+  
 
-  constructor() {}
+  @ViewChild('mapRef')
+  set mapRef(ref: ElementRef<HTMLElement>){
+    setTimeout (() => {
+      this.createMap(ref.nativeElement);
+    }, 500);
+  }
 
+
+
+
+  async createMap(ref: HTMLElement) {
+    this.map = await GoogleMap.create ({
+      id: 'my-cool-app',
+      apiKey: environment.mapsKey,
+      element: ref,
+      config : {
+        center: {
+          lat: 50.85045,
+          lng: 4.34878,
+
+        },
+        zoom:12,
+        styles: [
+          {
+            featureType: 'all',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { color: '#46d1c8' }, // Couleur des étiquettes
+            ],
+          },
+        ],
+          
+  
+        
+      },
+      forceCreate: true,
+
+    })
+  }
+  
 }
 
+      
