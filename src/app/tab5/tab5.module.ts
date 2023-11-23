865a6bcafc865d3e67bf2a2fb3adbc1c31db949b
Alex CM <a.cidmargues@students.ephec.be>
2023-11-23 17:21:13 +0100

    ajout log out
    bouton déconnecter fonctionne

--- src/app/tab5/tab5.module.ts
+++ src/app/tab5/tab5.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { Tab5PageRoutingModule } from './tab5-routing.module';
@@ -13,7 +13,8 @@ import { Tab5Page } from './tab5.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    Tab5PageRoutingModule
+    Tab5PageRoutingModule,
+    ReactiveFormsModule,
   ],
   declarations: [Tab5Page]
 })
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab5',
@@ -7,17 +11,42 @@ import { Router } from '@angular/router';
   styleUrls: ['./tab5.page.scss'],
 })
 export class Tab5Page implements OnInit {
+  userName: string;
+  credentials: FormGroup;
 
-  constructor(private router:Router) { }
+  constructor(
+    private router: Router,
+    private afAuth: AngularFireAuth,
+    private fb: FormBuilder,
+  ) {}
 
   ngOnInit() {
+    // Fetch the user's information on component initialization
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        // Assuming your user data in Firebase has a 'displayName' property
+        this.userName = user.displayName || 'User'; // Default to 'User' if display name is not set
+
+        // Initialize the form with the user's email and an empty password
+        this.credentials = this.fb.group({
+          email: [user.email || '', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(6)]],
+        });
+      }
+    });
   }
+
   goPersonal() {
     this.router.navigate(['/personal']);
-};
+  }
 
-goCar() {
-  this.router.navigate(['/car']);
-};
+  goCar() {
+    this.router.navigate(['/car']);
+  }
 
+  async logout() {
+    await this.afAuth.signOut(); // Use AngularFireAuth method to sign out
+    // You can navigate to the login page or any other desired page after logout
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
 }
