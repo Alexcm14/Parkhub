51bafcd6d70f2b537f1e47d1db229311bc8e846b
Alex CM <a.cidmargues@students.ephec.be>
2023-12-09 18:45:07 +0100

    supression véhicule possible

--- src/app/car/car.page.ts
+++ src/app/car/car.page.ts
@@ -1,11 +1,8 @@
-// car.page.ts
-
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/services/auth.service';
 import { from, take, switchMap } from 'rxjs';
 import { AngularFirestoreDocument } from '@angular/fire/compat/firestore';
-
 
 @Component({
   selector: 'app-car',
@@ -36,11 +33,11 @@ export class CarPage {
 
           console.log('User ID:', userId);
 
-          // Retourne les données supplémentaires de Firestore
+          // Retrieve additional data from Firestore
           return this.firestore.collection('user_data').doc(userId).valueChanges();
         } else {
           console.log('User is not logged in');
-          return from([]); // Chaîne qui continue
+          return from([]); // Empty observable
         }
       }),
       take(1)
@@ -52,7 +49,7 @@ export class CarPage {
         this.prenom = additionalData.prenom;
         this.telephone = additionalData.telephone;
 
-        // Charger les données de car_data
+        // Load car data
         this.loadCarData();
       } else {
         console.log('User data not found in Firestore.');
@@ -61,11 +58,11 @@ export class CarPage {
   }
 
   loadCarData() {
-    // récupérer les données de toutes les collections car_data
-    this.firestore.collection('car_data').doc(this.authService.uid).collection('car_data').valueChanges().subscribe((carData: any) => {
+    // Retrieve data from the 'car_data' collection
+    this.firestore.collection('user_data').doc(this.authService.uid).collection('car_data').valueChanges().subscribe((carData: any) => {
       if (carData) {
         console.log('Car Data:', carData);
-        // Mettre à jour la liste des véhicules
+        // Update the list of vehicles
         this.vehicles = carData;
       }
     });
@@ -73,10 +70,10 @@ export class CarPage {
 
   ajouterVehicule() {
     if (this.plaque && this.marque) {
-      // récupérer l'observable utilisateur
+      // Get the user observable
       const userObservable = this.authService.getLoggedInUserObservable();
   
-      // obtenir les données utilisateur
+      // Get user data
       userObservable.pipe(take(1)).subscribe((userData) => {
         if (userData) {
           const userId = this.authService.uid || userData['uid'];
@@ -89,14 +86,23 @@ export class CarPage {
               plaque: this.plaque,
             };
   
-            // Créer une nouvelle collection pour chaque véhicule
-            const userDocRef = this.firestore.collection('user_data').doc(userId).collection('car_data').doc();
+            // Generate a unique identifier for the vehicle (e.g., using a UUID library)
+            const vehicleId = this.generateUniqueId();
   
-            // Ajouter les données du véhicule à la collection
+            // Add the unique identifier to the vehicle data
+            user['id'] = vehicleId;
+  
+            // Create a new collection for each vehicle
+            const userDocRef = this.firestore.collection('user_data').doc(userId).collection('car_data').doc(vehicleId);
+  
+            // Add vehicle data to the collection
             userDocRef.set(user)
               .then(() => {
                 console.log('Vehicle added to Firestore successfully!');
-                // Charger les données de car_data après l'ajout
+                // Clear the input fields
+                this.plaque = '';
+                this.marque = '';
+                // Load car data after adding
                 this.loadCarData();
               })
               .catch((error) => {
@@ -108,7 +114,58 @@ export class CarPage {
     }
   }
   
+  // Generate a unique identifier (you may need to use a library for this)
+  generateUniqueId(): string {
+    // You can use a library like 'uuid' to generate a unique ID
+    // Example: import { v4 as uuidv4 } from 'uuid';
+    // return uuidv4();
+    // For simplicity, you can use a timestamp-based ID
+    return new Date().getTime().toString();
+  }
   
   
   
+  
+  
+
+  deleteVehicule(vehicle: any) {
+    console.log('Deleting vehicle:', vehicle); // Log the vehicle object
+    if (vehicle && vehicle.id) {
+      // Find the index of the vehicle in the local array
+      const index = this.vehicles.indexOf(vehicle);
+
+      if (index !== -1) {
+        // Remove the vehicle from the local array
+        this.vehicles.splice(index, 1);
+
+        // Update the local array
+        this.vehicles = [...this.vehicles];
+
+        // Delete the vehicle data from Firestore
+        this.deleteVehicleFromFirestore(vehicle);
+      }
+    }
+  }
+
+  deleteVehicleFromFirestore(vehicle: any) {
+    console.log('Deleting vehicle from Firestore. Vehicle ID:', vehicle.id);
+    if (vehicle && vehicle.id) {
+      // Get the user's ID (you may need to modify this depending on your authentication setup)
+      const userId = this.authService.uid;
+
+      if (userId) {
+        // Construct the Firestore path to the vehicle data
+        const vehiclePath = `user_data/${userId}/car_data/${vehicle.id}`;
+
+        // Delete the vehicle data from Firestore
+        this.firestore.doc(vehiclePath).delete()
+          .then(() => {
+            console.log('Vehicle deleted from Firestore successfully!');
+          })
+          .catch((error) => {
+            console.error('Error deleting vehicle from Firestore: ', error);
+          });
+      }
+    }
+  }
 }
